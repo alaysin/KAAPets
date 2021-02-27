436234e25d60d871caae1254aa32d6731c1318c1
Khlobystov Aleksandr <Aleksandr.Khlobystov@t-systems.com>
2021-02-27 16:44:57 +0100

    to pom.xml Spring dependecys added TestConfiguration added.

---
+++ src/test/java/org/levelup/TestConfiguration.java
@@ -0,0 +1,25 @@
+package org.levelup;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@Configuration
+@ComponentScan(basePackages = "org.levelup")
+public class TestConfiguration {
+
+    @Bean
+    public EntityManager entityManager(EntityManagerFactory factory) {
+        return factory.createEntityManager();
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory(){
+        return Persistence.createEntityManagerFactory("TestPersistenceUnit");
+    }
+}

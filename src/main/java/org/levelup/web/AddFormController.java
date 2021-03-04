6e6d1732904e7060f9292b06c7def5fcaee72461
Alex <no.remorse@yandex.ru>
2021-03-04 17:23:44 +0300

    persistence.xml updated

--- src/main/java/org/levelup/web/AddFormController.java
+++ src/main/java/org/levelup/web/AddFormController.java
@@ -1,12 +1,26 @@
 package org.levelup.web;
 
+import org.levelup.model.BreedersDAO;
+import org.levelup.model.PetsDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.persistence.EntityManager;
 
 @Controller
+@SessionAttributes("user-session")
 public class AddFormController {
+
+    @Autowired
+    private PetsDAO petsDAO;
+
+    @Autowired
+    private EntityManager manager;
+
     @PostMapping("/add")
     public String add(Model model, @RequestParam String nickname) {
         model.addAttribute("nickname", nickname);
--- src/main/java/org/levelup/web/StartPageController.java
+++ src/main/java/org/levelup/web/StartPageController.java
@@ -1,11 +1,12 @@
 package org.levelup.web;
 
-import org.levelup.model.Breeder;
-import org.levelup.model.Pets;
+import org.levelup.model.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 
 import java.time.LocalDate;
@@ -13,9 +14,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@SessionAttributes("user-session")
 public class StartPageController {
+
+    private final PetsDAO petsDAO;
+
+
+    public StartPageController(PetsDAO petsDAO) {
+        this.petsDAO = petsDAO;
+    }
+
     @GetMapping("/")
-    public String index(Model model, @RequestParam(defaultValue = "10") int count) {
+    public String index(
+            Model model,
+            @RequestParam(defaultValue = "10") int count,
+            @ModelAttribute("user-session") UserSession userSession
+    ) {
+        String title;
+        if (userSession.getUserLogin() == null) {
+            title = "Hello, anonymous!";
+        } else {
+            title = "Hello, " + userSession.getUserLogin() + "!";
+        }
+
+        model.addAttribute("pets", loadPets(10));
+        model.addAttribute("isAdmin", userSession.isAdmin());
+        model.addAttribute("isLoggedIn", userSession.getUserLogin() != null);
         model.addAttribute("subTitle", "Hello, you are using my Pet Booking Service.");
         model.addAttribute("title", "Pet Booking Service");
         model.addAttribute("error", "You are here, cause something goes wrong.");
@@ -26,16 +50,25 @@ public class StartPageController {
     }
 
     private List<Pets> loadPets(int count) {
-        ArrayList<Pets> pets = new ArrayList<>();
-        LocalDate date = LocalDate.of(2019, 1, 1);
-        Breeder breeder = new Breeder("test");
-        for (int i = 0; i < count; i++) {
-            pets.add(new Pets("nickname" + i, "breed" + i, date.plusDays(i), breeder));
-
-        }
-        Pets pets1 = new Pets("Test", "Test", date, breeder);
-        pets1.setReserverd(true);
-        pets.add(pets1);
-        return pets;
+        return petsDAO.findAll();
     }
+
+    @ModelAttribute("user-session")
+    public UserSession createUserSession() {
+        return new UserSession();
+    }
+
+//    private List<Pets> loadPets(int count) {
+//        ArrayList<Pets> pets = new ArrayList<>();
+//        LocalDate date = LocalDate.of(2019, 1, 1);
+//        Breeder breeder = new Breeder("test");
+//        for (int i = 0; i < count; i++) {
+//            pets.add(new Pets("nickname" + i, "breed" + i, date.plusDays(i), breeder));
+//
+//        }
+//        Pets pets1 = new Pets("Test", "Test", date, breeder);
+//        pets1.setReserverd(true);
+//        pets.add(pets1);
+//        return pets;
+//    }
 }
